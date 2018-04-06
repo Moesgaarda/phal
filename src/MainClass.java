31c1532d07b226621dd1ef3962f5415ae91e33fe
frederik Valdemar Schrøder <frederikhtxq12@gmail.com>
2018-04-06 11:24:10 +0200

    removed hardcoding of file path hopefully

--- src/MainClass.java
+++ src/MainClass.java
@@ -13,7 +13,7 @@ public class MainClass {
 
 	public static void main(String args[]) 
 	{
-		String fileName = "D:\\GitSource\\phal\\src\\PhalLangEx";
+		String fileName = "..\\phal\\src\\PhalLangEx";
         File file = new File(fileName);
         FileInputStream fis = null;
 		
