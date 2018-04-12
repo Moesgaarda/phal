d88bff83d1ca88775c6cc089ecf9e29dc18f271e
Daniel Moesgaard Andersen <moesgaarda@gmail.com>
2018-04-12 15:38:48 +0200

    Added infix and unary operator enums.

---
+++ src/enums/UnaryOperator.java
@@ -0,0 +1,20 @@
+package enums;
+
+public enum UnaryOperator{
+	NOT,
+	NEGATIVE;
+
+	@Override
+	public String toString() {
+		String stringRep = "";
+		switch (this) {
+			case NOT:
+				stringRep = "-";
+			case NEGATIVE:
+				stringRep = "!";
+			default:
+				stringRep = this.toString();
+		}
+		return stringRep;
+	}
+}
