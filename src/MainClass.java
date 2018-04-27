82c7c4475e809706848c4a9fb706d243180fd675
JP <jenspeturtrondarson@gmail.com>
2018-04-27 11:19:07 +0200

    Added some error handling in main and divided the main function

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,49 +1,84 @@
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
+
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.LinkedList;
 import java.util.List;
 
 public class MainClass {
+	
+	public static boolean parseErrorOcurred = false;
+	
 	public static List<CompilerError.Error> CompileErrors = new ArrayList<>();
 	public static List<CompilerError.Error> CompileWarnings = new ArrayList<>();
-	public static void main(String args[]) 
+	public static void main(String args[]) throws Exception
 	{
 
 		String fileName = "..\\Phal\\src\\PhalLangEx4";
         File file = new File(fileName);
-        FileInputStream fis = null;
-		
-		try {
-			fis = new FileInputStream(file);
-			ANTLRInputStream inputStream = new ANTLRInputStream(fis);
-			PhalLexer lexer = new PhalLexer(inputStream);
-			CommonTokenStream ts = new CommonTokenStream(lexer);
-			PhalParser parser = new PhalParser(ts);
-			PhalParser.ProgramContext cst = parser.program();
-			
-			System.out.println(cst.toStringTree());
-			BuildAST astBuilder = new BuildAST();
-			AstNode ast = astBuilder.visit(cst);
-			
-			PrettyPrinter pp = new PrettyPrinter();
-			pp.visit((ProgramNode) astBuilder.visitProgram(cst));
-			
-			BindingVisitor BV = new BindingVisitor();
-			BV.visit((ProgramNode) ast);
-			
-			int i = 2; 
-			//TODO SLET ER KUN TIL TESTING
-			if(i==3) {}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			
-		}
-	
-		
+        AstNode ast = ASTBuilder(new FileInputStream(file));
+					
+		BindingVisitor BV = new BindingVisitor();
+		BV.visit((ProgramNode) ast);	
 	}
 	
+	public static void Typechecking(AstNode ast) {
+		BindingVisitor bv = new BindingVisitor();
+		bv.visit((ProgramNode) ast);
+		
+		if(!CompileErrors.isEmpty()) {
+			PrintErrorsAndExit();
+		}
+	}
+	
+	public static AstNode ASTBuilder (InputStream is) throws Exception {
+		ANTLRInputStream inputStream = new ANTLRInputStream(is);
+		PhalLexer lexer = new PhalLexer(inputStream);
+		CommonTokenStream ts = new CommonTokenStream(lexer);
+		PhalParser parser = new PhalParser(ts);
+		parser.addErrorListener(new phalErrorListener());
+		PhalParser.ProgramContext cst = parser.program();
+		if(parseErrorOcurred) {
+			System.exit(0);
+		}
+		
+		BuildAST astBuilder = new BuildAST();
+		return astBuilder.visit(cst);
+	}
+	
+	public static void PrintErrorsAndExit() {
+		for(CompilerError.Error e : CompileErrors) {
+			System.out.println(e);
+		}
+		System.exit(0);
+	}
+	static class phalErrorListener implements ANTLRErrorListener {
+		
+		public void syntaxError(Recognizer<?, ?> recognizer, Object o, int i, int i1, String s, RecognitionException e) {
+			parseErrorOcurred = true;
+        }
+
+		@Override
+		public void reportAmbiguity(Parser arg0, DFA arg1, int arg2, int arg3, boolean arg4, BitSet arg5,
+				ATNConfigSet arg6) {
+			
+		}
+
+		@Override
+		public void reportAttemptingFullContext(Parser arg0, DFA arg1, int arg2, int arg3, BitSet arg4,
+				ATNConfigSet arg5) {
+			
+		}
+
+		@Override
+		public void reportContextSensitivity(Parser arg0, DFA arg1, int arg2, int arg3, int arg4, ATNConfigSet arg5) {
+			
+		}
+	}
 
 }
