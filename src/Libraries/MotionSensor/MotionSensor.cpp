9bd0b9515a484c08c49a6dc6d12e0c39b511d58d
Daniel Moesgaard <moesgaarda@gmail.com>
2018-05-11 13:31:23 +0200

    Added MotionSensor library.

---
+++ src/Libraries/MotionSensor/MotionSensor.cpp
@@ -0,0 +1,29 @@
+#include "Arduino.h"
+#include "MotionSensor.h"
+
+MotionSensor::MotionSensor(int pin)
+{
+    pinMode(pin, INPUT);
+    _pin = pin;
+}
+
+void MotionSensor::on()
+{
+    status = true;
+}
+
+void MotionSensor::off()
+{
+    status = false;
+}
+
+void MotionSensor::read()
+{
+    if (status = true)
+    {
+        if (digitalRead(_pin) != 0)
+            reading = true;
+        else
+            reading = false;
+    }
+}
