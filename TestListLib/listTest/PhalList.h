3c44318303dee05acb5318f0be143a590cffa412
frederik Valdemar Schrøder <frederikhtxq12@gmail.com>
2018-05-16 16:15:03 +0200

    PhalList implementation + test. seems to be working correctly

---
+++ TestListLib/listTest/PhalList.h
@@ -0,0 +1,90 @@
+#ifndef PhalList_h
+#define PhalList_h
+
+#include <stddef.h>
+template <class T>
+struct Node
+{
+  T data;
+  Node<T> *next;
+};
+
+
+template <typename T>
+class PhalList
+{
+  private:
+    Node<T> *root;
+    Node<T> *last;
+    int _size;
+  public:
+    PhalList();
+    int size();
+    void add(T _t);
+    T get(int index);
+    void remove(int index);
+};
+template <typename T>
+PhalList<T>::PhalList()
+{
+  root = new Node<T>();
+  last = root;
+  _size = 0;
+}
+template <typename T>
+int PhalList<T>::size()
+{
+  return _size;
+}
+template <typename T>
+void PhalList<T>::add(T _t)
+{
+  Node<T> *tmp = new Node<T>();
+  tmp->data = _t;
+  tmp->next = NULL;
+
+  last->next = tmp;
+  last = tmp;
+
+  _size++;
+}
+template <typename T>
+T PhalList<T>::get(int index)
+{
+  if(index > _size){
+    return false;
+  }
+  Node<T> *tmp = new Node<T>();
+  tmp = root->next;
+  for(int i = 0; i < index; i++)
+  {
+    tmp = root->next;
+  }
+  return tmp->data;
+}
+template <typename T>
+void PhalList<T>::remove(int index)
+{
+    if(index > _size){
+    return false;
+  }
+  Node<T> *removedNode = new Node<T>();
+  Node<T> *tmp = new Node<T>();
+  removedNode = root;
+  int i;
+  for(i = 0; i <= index; i++)
+  {
+    tmp = removedNode;
+    removedNode = removedNode->next;
+    if(i==index){
+      tmp->next = removedNode->next;
+    }
+  }
+  delete removedNode;
+  _size--;
+}
+
+
+
+
+#endif
---
+++ TestListLib/listTest/listTest.ino
@@ -0,0 +1,20 @@
+#include "PhalList.h"
+int a = 222;
+PhalList<int> list = PhalList<int>(); 
+
+void setup(){
+  Serial.begin(9600);
+  list.add(1);
+  list.add(2);
+  list.add(3);
+  int b = list.get(0);
+  Serial.println(b);
+  list.remove(0);
+  b = list.get(0);
+  
+  Serial.println(b);
+}
+void loop(){
+  
+}
+
