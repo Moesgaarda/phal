85074ae9045b8c3aa6ae742ff29138297610721d
frederik Valdemar Schrøder <frederikhtxq12@gmail.com>
2018-05-17 18:07:03 +0200

    motor.h Dont know what git is complaining about so reuploading this

---
+++ PhalLangEx4/Libraries/Motor/Motor.h
@@ -0,0 +1,14 @@
+#ifndef Motor_h
+#define Motor_h
+
+#include "Adt.h"
+
+class Motor: public Adt
+{
+    public:
+	    Motor(int pin);
+	    virtual void on();
+	    virtual void off();
+};
+
+#endif
